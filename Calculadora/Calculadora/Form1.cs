a1f9c2a5bde9111b3fb0324033d42cce77d39f78
Nicolas <152801721+nacolas@users.noreply.github.com>
2025-05-01 19:07:05 -0600

    Add files via upload
    El codigo chinguanguanchon de la calculadora

---
+++ Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        double primero;
+        double segundo;
+        double resultado;
+        string operacion;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "0";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "1";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "2";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "3";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "4";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "5";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "6";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "7";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "8";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "9";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + ".";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+           segundo = double.Parse(Pantalla.Text);
+
+            switch (operacion){
+                case "+":
+                    resultado = primero + segundo;
+                    Pantalla.Text = resultado.ToString();
+                    break;
+                case "-":
+                    resultado = primero - segundo;
+                    Pantalla.Text = resultado.ToString();
+                    break;
+                case "*":
+                    resultado = primero * segundo;
+                    Pantalla.Text = resultado.ToString();
+                    break;
+                case "/":
+                    resultado = primero / segundo;
+                    Pantalla.Text = resultado.ToString();
+                    break;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            operacion = "Raiz";
+            primero = double.Parse(Pantalla.Text);
+            resultado = primero;
+            Pantalla.Text = Math.Sqrt(primero).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            operacion = "+";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            operacion = "-";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            operacion = "*";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            operacion = "/";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pantalla.Clear();
+        }
+    }
+}
